03bb1a3b1a9133ff3802b9f69a7fccf61bf433a1
dev neko <tanawatplyiam@gmail.com>
2018-12-02 22:26:01 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
- $dsv_sql = ["", "localhost", "root", "Misaka677@", "database"];
+ $dsv_sql = ["", "localhost", "root", "Misaka677@", "MySQL"];
  $dsv_con = new mysqli($dsv_sql[1], $dsv_sql[2], $dsv_sql[3], $dsv_sql[4]);
  if ($dev_con->connect_error) {
   die("Connection failed: " . $conn->connect_error);
