2d568550239e330527a0209216fee10c506709ef
dev neko <tanawatplyiam@gmail.com>
2018-12-02 22:11:38 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
  $dsv_sql = ["", "localhost", "root", "password", "database"];
- $dsv_con = new mysql($dsv_sql[1], $dsv_sql[2], $dsv_sql[3], $dsv_sql[4]);
+ $dsv_con = new mysqli($dsv_sql[1], $dsv_sql[2], $dsv_sql[3], $dsv_sql[4]);
  if ($dev_con->connect_error) {
   die("Connection failed: " . $conn->connect_error);
  } else {}
