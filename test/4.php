14fd6ed87af27b224bb894efc24c7f3863299ff0
dev neko <tanawatplyiam@gmail.com>
2018-12-08 11:39:04 +0700

    Update 4.php

--- test/4.php
+++ test/4.php
@@ -16,29 +16,30 @@ $value = '3';
 		 array('f' => 'th','th' => '1'),
 		 array('f' => 'jp','jp' => '2'),
 	);
-	
 	$cars = json_encode($cars);
-	echo $cars;
-$cars = json_decode($cars, true);
-for($i=0,$f='';$i<5;$i++){
-	if(isset($cars[$i]['f'])){
-		if($cars[$i]['f'] != $land){
-			for($i=0,$f='';$i<5;$i++){
-				if(isset($cars[$i]['f'])){
-					$f = $f.json_encode(array(	'f' => $cars[$i]['f'],
-											$cars[$i]['f'] => $cars[$i][$cars[$i]['f']])
-					);
-											
-				} else {
-					$l = $i;
-					$i = 5;
-				}
-			} 
-			echo '['.$f.json_encode(array(	'f' => $land, $land => $value)).']';
+function land_insert($land,$value){
+	$cars = '[{"f":"th","th":"1"},{"f":"jp","jp":"2"}]';
+	$cars = json_decode($cars, true);
+	for($i=0,$f='';$i<5;$i++){
+		if(isset($cars[$i]['f'])){
+			if($cars[$i]['f'] != $land){
+				for($i=0,$f='';$i<5;$i++){
+					if(isset($cars[$i]['f'])){
+						$f = $f.json_encode(array(	'f' => $cars[$i]['f'],
+												$cars[$i]['f'] => $cars[$i][$cars[$i]['f']])
+						);
+												
+					} else {
+						$l = $i;
+						$i = 5;
+					}
+				} 
+				echo '['.$f.json_encode(array(	'f' => $land, $land => $value)).']';
+			} else {
+				$i = 9999999;
+			}
 		} else {
 			$i = 9999999;
 		}
-	} else {
-		$i = 9999999;
 	}
 }
