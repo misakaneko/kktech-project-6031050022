21d665c0d5bf84887b03fc75f5630008988032d6
dev neko <tanawatplyiam@gmail.com>
2018-12-08 08:59:46 +0700

    Update template-loading.php

--- z-include/template-loading.php
+++ z-include/template-loading.php
@@ -1,6 +1,10 @@
 <?php
-	$_SERVER['REQUEST_URI'];
 	function detect_url(){
+		$a = $_SERVER['REQUEST_URI'];
+		$b = '/'.TEMPLATE.'/';
+		if($a == $b){
+			echo 1;
+		} else {}
 	}
 	require_once(folder_template."index.php");
 ?>
