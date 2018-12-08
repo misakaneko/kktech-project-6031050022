d98615ec098e30c88700b20ecad7231aa9d82347
dev neko <tanawatplyiam@gmail.com>
2018-12-08 12:18:27 +0700

    Update mysql.php

--- z-include/mysql.php
+++ z-include/mysql.php
@@ -19,3 +19,11 @@
 			return "false";
 		}
 	}
+	function sql_update($id, $value){
+		$sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+		if ($conn->query($sql) === TRUE) {
+			echo "Record updated successfully";
+		} else {
+			echo "Error updating record: " . $conn->error;
+		}
+	}
