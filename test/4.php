d139689843cf051d3c5df6cf00a049181b5989c0
dev neko <tanawatplyiam@gmail.com>
2018-12-08 10:24:25 +0700

    Update 4.php

--- test/4.php
+++ test/4.php
@@ -9,4 +9,35 @@ function land($land, $value){
 	$cars = json_decode($cars, true);
 	return $cars['th'];
 }
-echo land('th','');
+//echo land('th','');
+
+	$cars = array(
+		 array('th' => '1'),
+		 array('jp' => '2'),
+	);
+	  
+	$cars = json_encode($cars);
+	$cars = json_decode($cars, true);
+	//echo $cars[0]['th'];
+	
+    $str = '{ 
+
+"players":[
+   {
+
+        "name":"Moldova",
+        "image":"/Images/Moldova.jpg",
+        "roll_over_image":"tank.jpg"
+   },
+   {
+
+        "name":"Georgia",
+        "image":"/Images/georgia.gif",
+        "roll_over_image":"tank.jpg"
+   } ]}';
+
+
+ $arr = json_decode($str, true);
+ $arrne['name'] = "dsds";
+ array_push( $arr['players'], $arrne );
+ print_r($arr);
