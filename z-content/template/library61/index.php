1264d233833051b7f2c2648318e0fec63982211b
dev neko <tanawatplyiam@gmail.com>
2018-12-08 09:45:35 +0700

    misakaneko

--- z-content/template/library61/index.php
+++ z-content/template/library61/index.php
@@ -2,6 +2,7 @@
 	if (session_status() == PHP_SESSION_NONE) {
 		session_start();
 	}
+	if(isset($_SESSION["TEMPLATE"])){} else {die();}
 	$a = $_SERVER['REQUEST_URI'];
 	$b = '/'.$_SESSION["TEMPLATE"].'/';
 	$c = '/'.$_SESSION["Z-TEMPLATE"].$_SESSION["TEMPLATE"].'/';
--- z-include/template-loading.php
+++ z-include/template-loading.php
@@ -1,11 +1,4 @@
 <?php
-	function detect_url(){
-		$a = $_SERVER['REQUEST_URI'];
-		$b = '/'.$_SESSION["TEMPLATE"].'/';
-		if($a == $b){
-			return 1;
-		} else {}
-	}
 	function get_header(){
 		require_once(folder_template.TEMPLATE."/header.php");
 	}
