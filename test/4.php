40f8b0544c57aba3035e16fb65f28c5c8d54849a
dev neko <tanawatplyiam@gmail.com>
2018-12-08 11:06:16 +0700

    Update 4.php

--- test/4.php
+++ test/4.php
@@ -17,23 +17,15 @@ function land($land, $value){
 	);
 	
 	$cars = json_encode($cars);
-	echo $cars;
+	//echo $cars;
 $cars = json_decode($cars, true);
 
 for($i=0;$i<5;$i++){
 	if(isset($cars[$i]['f'])){
-		echo $cars[$i][$cars[$i]['f']];
+		echo json_encode(array($cars[$i]['f'] => $cars[$i][$cars[$i]['f']]));
 	} else {
 		$i = 5;
 	}
 }
-
-	$cars = array(
-		 array('f' => 'th','th' => '1'),
-		 array('f' => 'jp','jp' => '2'),
-	)';
-	
-	$cars = json_encode($cars);
-	echo $cars;
 	//$cars = json_decode($cars, true);
 	//echo $cars[0]['th'];
