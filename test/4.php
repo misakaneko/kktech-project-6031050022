646d371afd804e23e9188a27558438ec546feb93
dev neko <tanawatplyiam@gmail.com>
2018-12-09 01:41:31 +0700

    Update 4.php

--- test/4.php
+++ test/4.php
@@ -2019,10 +2019,51 @@ $land = 'en';
 $value = '3';
 	$cars = json_encode($cars);
 function land_insert($land,$value){
+	global $earth;
+	$cars = '[{"f":"th","th":"1"},{"f":"jp","jp":"2"}]';
+	$cars = json_decode($cars, true);
+	$earth = json_decode($earth, true);
+	for($i=0;$i<200;$i++){
+		if(isset($earth[$i]['alpha2'])){
+			if($earth[$i]['alpha2'] == $land){
+			} else {}
+		} else {
+			$i = 9999999;
+			return 'false';
+		}
+	}
+	for($i=0;$i<5;$i++){
+		if(isset($cars[$i]['f'])){
+			if($cars[$i]['f'] != $land){
+				for($i=0,$f='';$i<5;$i++){
+					if(isset($cars[$i]['f'])){
+						$f = $f.json_encode(array(	'f' => $cars[$i]['f'],
+												$cars[$i]['f'] => $cars[$i][$cars[$i]['f']])
+						);
+												
+					} else {
+						$l = $i;
+						$i = 9999999;
+					}
+				} 
+				return '['.$f.json_encode(array(	'f' => $land, $land => $value)).']';
+			} else {
+				$i = 9999999;
+				return 'false';
+			}
+		} else {
+			$i = 9999999;
+			return 'false';
+		}
+	}
 }
 function land_update($land,$value){
 	global $earth;
 	$cars = '[{"f":"th","th":"1"},{"f":"jp","jp":"2"}]';
+	$cars = '';
+	if($cars == ''){
+		$cars = '[{"f":"null","null":"0"}]';
+	}
 	$cars = json_decode($cars, true);
 	$earth = json_decode($earth, true);
 	for($i=0;$i<200;$i++){
