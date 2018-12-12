e16dd09d81490daa40efb194a48b399f4a8c1f22
dev neko <tanawatplyiam@gmail.com>
2018-12-12 22:08:00 +0700

    Update api.php

--- z-content/template/library61/api.php
+++ z-content/template/library61/api.php
@@ -36,7 +36,7 @@ if(isset($_GET['t'])){
 		echo sql_update('0x77777', 'test', $contents);
 	} else {}
 	$Sdsd = json_decode($contents, true);
-	?>
+	?><meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
