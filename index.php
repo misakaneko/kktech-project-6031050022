e8bafc9c753b3d1c2425fc0b57a93f207ff13fd7
dev neko <tanawatplyiam@gmail.com>
2018-12-12 19:48:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	define("TEMPLATE","library61");
 	//define('ABSPATH', dirname(__FILE__).'/');
 	define('ABSPATH', '');
-	ECHO ABSPATH;
+	//ECHO ABSPATH;
 	//require_once "config.php";
 	//require_once "include.php";
 ?>
