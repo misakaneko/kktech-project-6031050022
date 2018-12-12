d449fa7fac39d8c57749b5ce7093b1cabba9941a
dev neko <tanawatplyiam@gmail.com>
2018-12-12 19:32:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,4 @@
 
 	require_once "config.php";
 	require_once "include.php";
-	
-	//sql_update(233, '西野カナ/トリセツ『ヒロイン失格』主題歌');
-	//echo sql_newdata('', '', '西野カナ/トリセツ『ヒロイン失格』主題歌 （Full Cover by Kobasolo & Lefty Hand Cream）');
-	//echo sql_update('OWQ2NT', '1', '777777');
-	//echo sql_load('OWQ2NT', 'fff', '');
-	echo land_update('jp','test','ced');
-	//echo sql_newdata(typeMultilanguage, typeMultilanguage, typeMultilanguage);
 ?>
